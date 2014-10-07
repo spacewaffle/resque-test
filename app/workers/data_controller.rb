ffc0f760ffde8eeeabbbbf6a1adae510571ed8ce
Jon Cheng <jon.syndicate@gmail.com>
2014-10-07 04:09:17 -0400

    removed some unnecessary stuff

--- app/workers/data_controller.rb
+++ app/workers/data_controller.rb
@@ -3,6 +3,11 @@ require 'logger'
 class DataController
   @queue = :data_queue
 
+  #start redis server with this
+  #redis-server
+
+  #remember to start resque workier with this
+  #TERM_CHILD=1 QUEUES=* rake environment resque:work
 
   def self.perform
     Rails.logger.info "sleeping 10 sec, then storing something in redis"
--- lib/tasks/poop.rake
+++
@@ -1,6 +0,0 @@
-require "resque/tasks"
-
-task "resque:setup" => :environment
-
-#to run this rake task, use this command
-#TERM_CHILD=1 QUEUES=* rake resque:work
